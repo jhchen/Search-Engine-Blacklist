3821a2506b04ae0dd2d021808761900db73e5567
yayo23 <yayo23@gmail.com>
2011-01-13 06:48:20 -0500

    Removed uncessary code, fixed validity checker and commented a tad

--- chrome/common.js
+++ chrome/common.js
@@ -1,25 +1,30 @@
 function validateUri(uri) {
 	var host = parseUri(uri);	
-	switch(host.host){
-		case "":
-			host = host.source;
-			break;
-		default:
-			host = host.host;
-	}
+   
+    host = host.host;
+    if (host.host == "") {
+        // Might have to grab source member depending
+        // on what parseUri returns
+        host = host.source; 
+    }
 	
 	var parts = host.split(".");
     var numParts = parts.length-1; 
 	var offset = 0;
+
+    /* Catch any hosts with a tld of `.co.foo` */
     if (parts[numParts-1] == "co"){
 		offset = 1;
     }
 	var tld = parts.splice(numParts-offset, numParts).join(".");
 	var host = parts.pop();
 	var valid = false;
-    if (tld.length > 0 && host+'.'+tld.match(/^([a-z0-9\-_]+\.)+[a-z0-9\-_]{1,4}$/i)) {
+    /* Determine if this is a valid domain name
+       for use by the validity checker in options.js */
+    if (tld.length > 0 && (host+'.'+tld).match(/^([a-z0-9\-_]+\.)+[a-z0-9\-_]{1,4}$/i)) {
 		valid = true;
 	}
+    // Grab whatever remains for the subdomain, if anything
 	var subdomain = (numParts-(1+offset) > 0) ? parts.join(".") : "";
 	(subdomain.indexOf("www") == 0) && (subdomain = subdomain.substring(4));
 	var domain = (tld.length > 0) ? [host, tld].join(".") : host;
