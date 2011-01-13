b4396ca2af1e2b5ea570f0a564dc803247ff5c5f
Jason Chen <jhchen7@gmail.com>
2011-01-13 02:45:04 -0800

    added few more test cases

--- tests/validate_uri_test.js
+++ tests/validate_uri_test.js
@@ -1,19 +1,26 @@
 $(document).ready(function(){
     test("Parsing Uris", function() {
     	var testData = {
-    	    //Should all be good, parseUri should do all this for us
     		"http://www.google.com": {host: 'google', tld: 'com', subdomain: '', domain: 'google.com', full: 'google.com', valid: true},
     		"https://www.google.com/": {host: 'google', tld: 'com', subdomain: '', domain: 'google.com', full: 'google.com', valid: true},
     		"http://www.google.com/path": {host: 'google', tld: 'com', subdomain: '', domain: 'google.com', full: 'google.com', valid: true},
     		"http://google.com": {host: 'google', tld: 'com', subdomain: '', domain: 'google.com', full: 'google.com', valid: true},
-    		"http://sub.google.com": {host: 'google', tld: 'com', subdomain: 'sub', domain: 'google.com', full: 'sub.google.com', valid: true}
-    	
-    		//And now for our parser
+    		"http://sub.google.com": {host: 'google', tld: 'com', subdomain: 'sub', domain: 'google.com', full: 'sub.google.com', valid: true},
+    		"google.com": {host: 'google', tld: 'com', subdomain: '', domain: 'google.com', full: 'google.com', valid: true},
+    		"http://*.google.com": {host: 'google', tld: 'com', subdomain: '*', domain: 'google.com', full: '*.google.com', valid: true},
+    		
+    		"foobar": {valid: false},
+    		"http://www.goog$le.com/": {valid: false}
     	};
     	
     	for (var key in testData) {
     		var result = validateUri(key);
-    		deepEqual(result, testData[key], key);
+    		if (testData[key].valid) {
+    			deepEqual(result, testData[key], key);
+    		}
+    		else {
+    			equal(result.valid, testData[key].valid, key);
+    		}
     	}
     });
 });
